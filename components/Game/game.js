c7c35e1a21810c5246cc85e8bfdf6a6d7c8df084
Richard Xu <richardxu@berkeley.edu>
2024-08-30 11:39:27 -0500

    Added start timer logic moved clickability to Player level

--- components/Game/game.js
+++ components/Game/game.js
@@ -1,28 +1,35 @@
 import { useState, useEffect } from 'react';
 import Player from '../Player/player';
-import { Button, Text, View, StyleSheet } from 'react-native';
+import { Button, Text, View, StyleSheet, TouchableOpacity } from 'react-native';
 
 const Game = ({ starting_increment, starting_bank })  => {
     const startingNumberOfPlayers = [0, 1, 2, 3];
-    const [currentTurn, setTurn] = useState(0);
+    const [currentTurn, setTurn] = useState(null);
 
     const endTurnCb = () => {
-        console.log("endTurn CB called")
         setTurn(playerTurn => (playerTurn + 1) % 4);
-        console.log("playerTurn: " + currentTurn)
     }
 
     return (
         <View style={styles.container}>
             {startingNumberOfPlayers.map(player => {
                 return (
-                    <Player 
-                        id={player} 
-                        starting_increment={starting_increment} 
-                        starting_bank={starting_bank} 
-                        endTurnCb={endTurnCb}
-                        my_turn={currentTurn === player}
-                    />
+                    <TouchableOpacity disabled={!((currentTurn === null) || currentTurn === player)} style={styles.button} onPress={() => { 
+                        if(currentTurn === null) {
+                            setTurn(player);
+                        } else{
+                            setTurn(playerTurn => (playerTurn + 1) % 4);
+                        }
+                    }}>
+                        <Player 
+                            id={player} 
+                            starting_increment={starting_increment} 
+                            starting_bank={starting_bank} 
+                            endTurnCb={endTurnCb}
+                            my_turn={currentTurn === player}
+                        />
+                    </TouchableOpacity>
+
                 );
             
     })}
@@ -31,6 +38,15 @@ const Game = ({ starting_increment, starting_bank })  => {
 }
 
 const styles = StyleSheet.create({
+    button: { 
+        width: '50%', 
+        height: '50%', 
+        margin: '0', 
+        padding: '0', 
+        backgroundColor: 'white', 
+        borderColor: 'black', 
+        borderWidth: 1.5
+      },
     container: {
       display: 'flex', 
       flexDirection: 'row',
--- components/Player/player.js
+++ components/Player/player.js
@@ -1,41 +1,27 @@
 import { useState, useEffect } from 'react';
 import Timer from '../Timer/timer';
-import { Button, Text, View, StyleSheet, TouchableOpacity } from 'react-native';
+import { Button, Text, View, StyleSheet } from 'react-native';
 import { Card, Icon } from '@rneui/themed';
 
 
 
 const Player = ({ id, starting_increment, starting_bank, endTurnCb, my_turn })  => {
-  console.log("PlayerID: " + id + " my_turn: " + my_turn)
 
   const out_of_time_cb = () => {
     endTurnCb();
   };
 
     return (
-    <TouchableOpacity disabled={!my_turn} style={styles.container} onPress={() => { 
-      console.log("clicked player: " + id)
-      endTurnCb(); }}>
     <Card containerStyle={styles.item}>
-      <View style={styles.item}>
+      <View style={styles.view}>
         <Text style={styles.item}>Player {id}</Text>
         <Timer player_id={id} starting_increment={starting_increment} starting_bank={starting_bank} timer_running={my_turn} out_of_time_cb={out_of_time_cb}/>
       </View>
     </Card>
-    </TouchableOpacity>
     );
 }
 
 const styles = StyleSheet.create({
-    container: { 
-      width: '50%', 
-      height: '50%', 
-      margin: '0', 
-      padding: '0', 
-      backgroundColor: 'white', 
-      borderColor: 'black', 
-      borderWidth: 1.5
-    },
     item: {
       borderWidth: 0, // Remove Border
 
@@ -47,7 +33,12 @@ const styles = StyleSheet.create({
       elevation: 0, // Remove Shadow for Android
       textAlign: 'center', // <-- the magic
       fontWeight: 'bold',
-    }
+    },
+    view: {
+      backgroundColor: 'white',
+      height: "100%",
+      width: "100%",
+    },
   });
   
 
