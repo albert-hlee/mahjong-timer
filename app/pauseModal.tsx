16634349884125c4c2367e7a8b6a272872771cf0
Albert Lee <albert.hyunchul@gmail.com>
2024-10-01 23:03:10 -0700

    removed isPresented and router handling here, might bring back for pause reset potentially, addded potential reset button

--- app/pauseModal.tsx
+++ app/pauseModal.tsx
@@ -1,10 +1,9 @@
-import { Link, router} from 'expo-router';
+import { Link } from 'expo-router';
 import { View, StyleSheet, Text, Pressable, Modal } from 'react-native';
 
 // TODO: specify the types to boolean and function
-export default function PauseMenu( {isVisible, onClose} : any ) {
-  const isPresented = router.canGoBack();
-
+export default function PauseMenu( {isVisible, onClose, onReset} : any ) {
+//   const isPresented = router.canGoBack();
   return (
     <Modal
         animationType='slide' 
@@ -14,6 +13,9 @@ export default function PauseMenu( {isVisible, onClose} : any ) {
       <View style={styles.container}>
         <Text style={styles.title}>Pause screen</Text>
         <Link style={styles.link} replace href="/">Home Page</Link>
+        <Pressable onPress={onReset}>
+            <Text style={styles.link}>Reset</Text>
+        </Pressable>        
         <Pressable onPress={onClose}>
             <Text style={styles.link}>Back</Text>
         </Pressable>
