e9c0b9a04b0886caf88f0149ac8c2b9767985f5c
Albert Lee <albert.hyunchul@gmail.com>
2024-10-08 15:50:59 -0700

    linted

--- components/MainMenu/mainMenu.js
+++ components/MainMenu/mainMenu.js
@@ -1,125 +1,147 @@
-import { useState, useEffect } from 'react';
-import Timer from '../Timer/timer';
+import { useState, useEffect } from "react";
 
-import { Button, Keyboard, Text, TextInput, TouchableWithoutFeedback,TouchableOpacity,  View, ScrollView, StyleSheet } from 'react-native';
-import { Card, Icon } from '@rneui/themed';
-import { Link, useRouter } from 'expo-router';
+import {
+  Button,
+  Keyboard,
+  Text,
+  TextInput,
+  TouchableWithoutFeedback,
+  TouchableOpacity,
+  View,
+  ScrollView,
+  StyleSheet,
+} from "react-native";
+import { Card, Icon } from "@rneui/themed";
+import { Link, useRouter } from "expo-router";
+import Timer from "../Timer/timer";
 
-const MainMenu = ({})  => {
-    const [startingNumberOfPlayers, setNumberOfPlayers] = useState(4);
-    const [startingNumberOfRounds, setNumberOfRounds] = useState(4);
-    const [startingPoints, setPoints] = useState(25000);
-    const [baseTime, setBaseTime] = useState(60);
-    const [incrementTime, setIncrementTime] = useState(15); // default values in mahjong soul kek
+const MainMenu = ({}) => {
+  const [startingNumberOfPlayers, setNumberOfPlayers] = useState(4);
+  const [startingNumberOfRounds, setNumberOfRounds] = useState(4);
+  const [startingPoints, setPoints] = useState(25000);
+  const [baseTime, setBaseTime] = useState(60);
+  const [incrementTime, setIncrementTime] = useState(15); // default values in mahjong soul kek
 
-    const router = useRouter();
+  const router = useRouter();
 
-    const handlePress = () => {
-      router.push({
-        pathname: "/gameView",
-        params: {
-          starting_increment: incrementTime,
-          starting_bank: baseTime,
-        },
-      });
-    };
+  const handlePress = () => {
+    router.push({
+      pathname: "/gameView",
+      params: {
+        starting_increment: incrementTime,
+        starting_bank: baseTime,
+      },
+    });
+  };
 
-    return (
-      <View style={styles.parentContainer}>
-        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-          <View style={styles.menu}>
-            <Text style={styles.title}>Game Settings</Text>
-            <Text style={styles.item}>Starting Number of Players</Text>
-            <TextInput
+  return (
+    <View style={styles.parentContainer}>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View style={styles.menu}>
+          <Text style={styles.title}>Game Settings</Text>
+          <Text style={styles.item}>Starting Number of Players</Text>
+          <TextInput
             style={styles.input}
             onChangeText={setNumberOfPlayers}
             inputMode="numeric"
-            keyboardType='numeric'
-            >{startingNumberOfPlayers}</TextInput>
-            <Text style={styles.item}>Rounds</Text>
-            <TextInput
+            keyboardType="numeric"
+          >
+            {startingNumberOfPlayers}
+          </TextInput>
+          <Text style={styles.item}>Rounds</Text>
+          <TextInput
             style={styles.input}
             onChangeText={setNumberOfRounds}
             inputMode="numeric"
-            keyboardType='numeric'
-            type="number">{startingNumberOfRounds}</TextInput>
-            <Text style={styles.item}>Starting Points</Text>
-            <TextInput
+            keyboardType="numeric"
+            type="number"
+          >
+            {startingNumberOfRounds}
+          </TextInput>
+          <Text style={styles.item}>Starting Points</Text>
+          <TextInput
             style={styles.input}
             onChangeText={setPoints}
             inputMode="numeric"
-            keyboardType='numeric'
-            type="number">{startingPoints}</TextInput>
-            <Text style={styles.item}>Base Time</Text>
-            <TextInput
+            keyboardType="numeric"
+            type="number"
+          >
+            {startingPoints}
+          </TextInput>
+          <Text style={styles.item}>Base Time</Text>
+          <TextInput
             style={styles.input}
             onChangeText={setBaseTime}
             inputMode="numeric"
-            keyboardType='numeric'
-            type="number">{baseTime}</TextInput>
-            <Text style={styles.item}>Increment Time</Text>
-            <TextInput
+            keyboardType="numeric"
+            type="number"
+          >
+            {baseTime}
+          </TextInput>
+          <Text style={styles.item}>Increment Time</Text>
+          <TextInput
             style={styles.input}
             onChangeText={setIncrementTime}
             inputMode="numeric"
-            keyboardType='numeric'
-            type="number">{incrementTime}</TextInput>
-            <TouchableOpacity style={styles.button} onPress={handlePress}>
-              <Text>Start Game</Text>
-            </TouchableOpacity>
-          </View>
-        </TouchableWithoutFeedback>
-      </View>
-    );
-}
+            keyboardType="numeric"
+            type="number"
+          >
+            {incrementTime}
+          </TextInput>
+          <TouchableOpacity style={styles.button} onPress={handlePress}>
+            <Text>Start Game</Text>
+          </TouchableOpacity>
+        </View>
+      </TouchableWithoutFeedback>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
+  button: {
+    alignItems: "center",
+    backgroundColor: "#4CAF50",
+    borderRadius: 5,
+    padding: 10,
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  input: {
+    borderWidth: 1,
+    height: 40,
+    margin: 12,
+    padding: 10,
+  },
+  item: {
+    borderWidth: 0, // Remove Border
 
-    item: {
-      borderWidth: 0, // Remove Border
+    shadowColor: "rgba(0,0,0, 0.0)", // Remove Shadow for iOS
+    shadowOffset: { height: 0, width: 0 },
+    shadowOpacity: 0,
+    shadowRadius: 0,
 
-      shadowColor: 'rgba(0,0,0, 0.0)', // Remove Shadow for iOS
-      shadowOffset: { height: 0, width: 0 },
-      shadowOpacity: 0,
-      shadowRadius: 0,
-      
-      elevation: 0, // Remove Shadow for Android
-      textAlign: 'center', // <-- the magic
-      fontWeight: 'bold',
-    },
-    parentContainer: {
-      backgroundColor: 'white',
-      height: "100%",
-      width: "100%",
-      padding: '10%',
-    },
-    menu: {
-      height: "100%",
-      width: "100%",
-    },
-    input: {
-      height: 40,
-      margin: 12,
-      borderWidth: 1,
-      padding: 10,
-    },
-    title: {
-      textAlign: 'center',
-      fontStyle: 'bold',
-      fontSize: '30'
-    },
-    button: {
-      backgroundColor: '#4CAF50',
-      padding: 10,
-      borderRadius: 5,
-      alignItems: 'center',
-    },
-    buttonText: {
-      color: '#fff',
-      fontSize: 18,
-      fontWeight: 'bold',
-    },
-  });
-  
+    elevation: 0, // Remove Shadow for Android
+    textAlign: "center", // <-- the magic
+    fontWeight: "bold",
+  },
+  menu: {
+    height: "100%",
+    width: "100%",
+  },
+  parentContainer: {
+    backgroundColor: "white",
+    height: "100%",
+    padding: "10%",
+    width: "100%",
+  },
+  title: {
+    fontSize: "30",
+    fontStyle: "bold",
+    textAlign: "center",
+  },
+});
 
 export default MainMenu;
